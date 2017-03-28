ebc40d6d8946ca95a1b67f5236e5c8210f420081
Sebastian Straube <github@straube.ch>
2017-03-28 23:34:57 +0200

    patch

--- src/main/java/ch/cloudlabx/chat_server_websocket/ServerStart.java
+++ src/main/java/ch/cloudlabx/chat_server_websocket/ServerStart.java
@@ -7,6 +7,10 @@ import java.io.InputStreamReader;
 public class ServerStart {
 
     public static void main(String[] args){
+        System.out.print("length args: " + args.length);
+        System.out.print("args[0]" + args[0]);
+        System.out.print("args[1]" + args[1]);
+
         String hostIp = args[0]; //localhost
         int port = new Integer(args[1]); //port
         String rootContext = "/room";
